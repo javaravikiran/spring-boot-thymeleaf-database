54010d006a1046f29dd6b3c8eea041fe071ad86f
javaravikiran <ravikiran.namana@gmail.com>
2021-09-22 00:38:08 +0530

    adding eureka client config changes

--- src/main/java/com/javaravi/SpringBootThymeleafDatabaseApplication.java
+++ src/main/java/com/javaravi/SpringBootThymeleafDatabaseApplication.java
@@ -2,10 +2,13 @@ package com.javaravi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootThymeleafDatabaseApplication {
-
+	
+//Booting application from here
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafDatabaseApplication.class, args);
 	}
